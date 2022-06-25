d813bb73bc55c36672b4bcd0c842c79e287ff29e
Edoardo Panichi <edoardo.panichi99@gmail.com>
2022-06-25 12:25:04 +0200

    First function to play a random legal move

--- src/chess_functions.py
+++ src/chess_functions.py
@@ -6,9 +6,18 @@ class chessboard:
         self.board = chess.Board()
         
     def random_legal_move(self):
-        legal_moves = list(self.board.legal_moves)
-        rand_move = np.random.randint(len(legal_moves))
-        self.board.push(legal_moves[rand_move])
+        if self.board.is_game_over() == False:
+            legal_moves = list(self.board.legal_moves)
+            
+            rand_move = np.random.randint(len(legal_moves))
+            self.board.push(legal_moves[rand_move])
+            game_finished = False
+                
+        else: 
+            print(self.board.outcome())
+            game_finished = True
+            
+        return game_finished
         
 
     
