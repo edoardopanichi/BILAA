fae0d73a17a1e52e782d8b94dc5d796303176f4d
Edoardo Panichi <@epanichi>
2022-08-04 11:36:02 +0200

    added a main.py. Because jupiter notebook might be the cause of errors for long trainings.

--- src/store_load_models.py
+++ src/store_load_models.py
@@ -1,9 +1,8 @@
 import pickle
         
 # set the variable 'file_empty' to True when trained_models.pkl is empty or when you what to delete the content. 
-# To delete the content insert 'yes' when prompt ask if you want to store (related to the function 
-# store_models).    
-def load_store_models(file_empty=False, agent_copy=None):
+# To delete the content assign 'yes' to 'store'.  
+def load_store_models(file_empty=False, agent_copy=None, store=str("no")):
     save_filename = "keras_models/trained_models.pkl"
 
     # if the file is not empty, we load its content into a variable.
@@ -12,9 +11,6 @@ def load_store_models(file_empty=False, agent_copy=None):
             trained_models = pickle.load(f)
         
     # Thanks to 'store' we can decide whether to store or not the just trained model.
-    store = str("no")
-    store = input("\nUpdate the description of the model!!\nDo you want to save this model to the external file? \nyes or no")
-
     if store == "yes":
         if file_empty:
             trained_models = []
